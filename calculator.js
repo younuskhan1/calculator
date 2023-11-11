1d8a72910ad4586f9c660f3ac95c7267c8efc474
younuskhan1 <md.younuskhan412@gmail.com>
2023-11-11 16:59:48 +0600

    the last push

--- calculator.js
+++ calculator.js
@@ -1,15 +1,19 @@
 const display = document.querySelector(".display");
 // new things
 const buttons = document.querySelectorAll("button");
-const specialchars = ["%", "+", "-", "*", "/", "="];
+const specialChars = ["%", "+", "-", "*", "/", "="];
 let output = "";
 
 // add eventlisteners to the buttons to call the calculate button. 
 // define function to calculate based on button clicked
 const calculate = (btnValue) => {
+
     if (btnValue === "=" && output !== "") {
         // if output has "%" then replace "%" with "/100" before evaluating. 
+        // eval() method is used to execute the code after replacing "%" with "/100".
+        // without eval() method the line will not be executed just will be replaced.
         output = eval(output.replace("%", "/100"));
+        // console.log(output); 
     } else if (btnValue === "AC") {
         output = "";
     } else if (btnValue === "DEL") {
@@ -17,14 +21,17 @@ const calculate = (btnValue) => {
         // output = output.toString().slice(0, -1); same as exactly
         output = output.toString().slice(0, output.length - 1);
         // if output is empty and button is special characters then return 
-    } else if (output === "" && specialchars.includes(btnValue)) {
+    } else if (output === "" && specialChars.includes(btnValue)) {
         return;
 
     } else {
+        // otherwise btnvalue will be added to the output value. 
         output += btnValue;
     }
+    // this line will be executed at last after reviewing above conditions. 
     display.value = output;
 }
+
 buttons.forEach((button) => {
     // button click listener calls calculate function with the dataset value as arguments 
     button.addEventListener("click", (e) => calculate(e.target.dataset.value));
